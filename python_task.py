7e9da9f7936bea136772fe08d7d4343fefc6fede
gurman14 <60088608+gurman14@users.noreply.github.com>
2021-04-08 19:11:22 +0530

    Add files via upload

---
+++ python_task.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df=pd.read_excel("employee__1_.xls")
+
+df['Date of Birth'] = pd.to_datetime(df['Date of Birth'])
+df['Date of Joining'] = pd.to_datetime(df['Date of Joining'])
+
+x=df.groupby('Quarter of Joining').apply(pd.DataFrame.sort_values, 'Date of Birth')
+
+dict1={}
+for i in range(0,len(x)):
+    if x["Quarter of Joining"][i] in dict1.keys():
+        dict1[x["Quarter of Joining"][i]].append(x["First Name"][i])
+    else:
+        dict1[x["Quarter of Joining"][i]]=[x["First Name"][i]]
+
+print(dict1)
+
+
+
+
+
+
+
